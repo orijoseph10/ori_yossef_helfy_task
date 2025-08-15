6517da1e0f89742293a7664f0a912fc7200f7013
josephori <josephori30@gmail.com>
2025-08-15 13:20:13 +0300

    added zod validation

--- packages/backend/interfaces/Task.ts
+++ packages/backend/interfaces/Task.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 export interface Task {
   id: number;
   title: string;
@@ -6,3 +7,19 @@ export interface Task {
   createdAt: Date;
   priority: "low" | "medium" | "high";
 }
+
+export const createTaskSchema = z.object({
+  title: z.string().min(1, "Title cannot be empty."),
+  description: z.string().min(1, "Description cannot be empty."),
+  priority: z.enum(["low", "medium", "high"]),
+});
+
+export const updateTaskSchema = z.object({
+  title: z.string().min(1, "Title cannot be empty.").optional(),
+  description: z.string().min(1, "Description cannot be empty.").optional(),
+  priority: z.enum(["low", "medium", "high"]).optional(),
+  completed: z.boolean().optional(),
+});
+
+export type CreateTaskDto = z.infer<typeof createTaskSchema>;
+export type UpdateTaskDto = z.infer<typeof updateTaskSchema>;
