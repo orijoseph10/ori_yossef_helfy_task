7ed56d62a4395abe78fc839f4e316886fa7ac6d8
josephori <josephori30@gmail.com>
2025-08-15 13:11:49 +0300

    changed port

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -2,13 +2,13 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 
 const app: Express = express();
-const port: number = 3000;
+const port: number = 4000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/api", (req: Request, res: Response) => {
-  res.json({ message: "Hello from the TypeScript backend!" });
+  res.json({ message: "Hello World" });
 });
 
 app.listen(port, () => {
