d7e9db7e9b6410d8c7595ca147a5762d604f3fce
josephori <josephori30@gmail.com>
2025-08-15 14:18:38 +0300

    added react toastify

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +13,18 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </QueryClientProvider>
   </StrictMode>
 );
